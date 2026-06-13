968b457d8a0c41e466e4af79dcb3cd3e6626464a
abdulhakimarrauf-sys <abdulhakimarrauf@gmail.com>
2026-06-13 23:49:13 +0700

    Implement matrix operations in matriks.py
    Added matrix operations including transpose, multiplication, and determinant calculation.

--- matriks.py
+++ matriks.py
@@ -1,1 +1,32 @@
+import percobaan as pn
 
+if __name__ == "__main__":
+    print("=== UJI COBA MODUL MATRIKS ===\n")
+
+    A = pn.RAHA([[1, 2], 
+                [3, 4]])
+    B = pn.RAHA([[5, 6], 
+                [7, 8]])
+    print("Matriks A:")
+    print(A)
+    print("\n")
+    print("Matriks B:")
+    print(B)
+
+    print("\n")
+
+    print("1. Hasil Transpos Matriks A:")
+    A_T = A.transpos()
+    print(A_T)
+
+    print("\n")
+
+    print("2. Hasil Perkalian Matriks (A x B):")
+    Hasil_Kali = A.kali(B)
+    print(Hasil_Kali)
+
+    print("\n")
+
+    print("3. Hasil Determinan Matriks A (2x2):")
+    det_A = A.determinan_2x2()
+    print(f"Nilai Determinan = {det_A}")
